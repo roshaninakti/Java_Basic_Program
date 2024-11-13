001d22d780afda155ca016f7da0dd46cb4106ca1
Roshani Nakti <109640992+roshaninakti@users.noreply.github.com>
2024-11-13 09:38:32 +0530

    Add files via upload

---
+++ Day_Two.java
@@ -0,0 +1,73 @@
+// To check if given integer old and even 
+/* class Day_Two{
+    public static void main(String arg[])
+    {
+      int num = 58 ;
+         if(num%2==0)
+         {
+           System.out.println(num + " is Even ");
+         }
+        else
+        {
+            System.out.println(num + " is Old");
+        }
+    }
+}
+    */
+  //---- ---------------------------------- End-----------------------------------------
+  //   Find the Largest of three Numbers
+ /*  public class Day_Two {
+       public static void main(String[] args) 
+       {
+        int numOne = 12 ;
+        int numTwo = 15 ;
+        int numThree = 18 ;
+      if(numOne>=numTwo && numOne>=numTwo)
+        {
+        System.out.println(numOne + " id largest");
+        }
+        else if(numTwo>=numOne && numTwo>=numThree)
+        {
+           System.out.println(numTwo+ " is largest");
+        }
+       else
+       {
+         System.out.println(numThree + " is largest");
+       }
+       
+    }
+    
+  }
+    */
+//----------------------------------------End--------------------------------------
+// To check prime number
+/* public class Day_Two {
+
+    public static void main(String[] args) {
+        int num = 3 ;
+        int count = 0;
+
+        if(num>1)
+        { 
+            for(int i = 1 ; i<=num; i++)
+            {
+                 if(num%i ==0)
+                  count++;
+
+            }
+            if(count==2)
+              {
+               System.out.println(num + " is prime number ");
+             }
+              else{
+                System.out.println(num + " is not prime number");
+            }
+        }    
+    else
+       {
+            System.out.println(num + " is not prime number");
+        }
+    }
+}
+    */
+//----------------------------------End----------------------------------------------    
