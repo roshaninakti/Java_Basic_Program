4996b9c990e9157bf03d9717d80faa5a8a530de2
Roshani Nakti <roshaninakti98@gmail.com>
2025-05-22 10:13:10 +0530

    Add files via upload

---
+++ Day_1.java
@@ -0,0 +1,21 @@
+public class Day_1 {
+//     1. Write Hello World Program in Java
+      
+// static void helloMessage(){
+//             System.out.println("Hello World");
+//       }
+//          public static void main(String[] args) {
+//           helloMessage();
+//          }
+
+
+// 2. Write a Program in Java to Add two Numbers.
+
+static void addTwoNum(int a , int b){
+          System.out.println("Addition of two number :" + (a+b) );
+}
+  public static void main(String[] args) {
+          addTwoNum(10,20);
+  }
+
+}
