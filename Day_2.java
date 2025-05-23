28075d71a6cab3b2f2b257568c5230add35acad8
Roshani Nakti <roshaninakti98@gmail.com>
2025-05-23 10:21:07 +0530

    Add files via upload

---
+++ Day_2.java
@@ -0,0 +1,29 @@
+public class Day_2 {
+
+//           public static void main(String[] args) {
+//                     String name;;
+//                 System.out.println("Enter a name :");
+//                  Scanner sn = new Scanner (System.in);
+//                   name=sn.next();
+//                 }
+// 4. Multiply of two numbers 
+// public static void main(String[] args) {
+//           float num1 = 12.5f;
+//           float num2 = 12.59f;
+//           float mul = num1*num2;
+//           System.out.println(mul);
+// }
+// Java Program to Swap Two Numbers
+public static void main(String[] args) {
+          int num1 = 10 ;
+          int num2 =20;
+          int temp;
+          System.out.println("Enter  "  +  num1  +" "  +   num2  +   "   a number before swpping ");
+          temp=num1;
+          num1=num2;
+          num2=temp;
+          System.out.println("Getting "  + num1 + " " +   num2 +  "  a numbers swaping ");
+
+}
+
+}
